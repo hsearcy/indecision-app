4188e675c8fd60676ede6b3784077c023900b2d2
hsearcy <hfsearcy@gmail.com>
2018-11-30 06:57:52 -0500

    up

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 console.log("App.js is running!");
 
-var app = {
+const app = {
     title: "Indecision App",
     subtitle: "DON'T put your life in the hands of a computer",
     options: ["one", "two"]
 }
 
-var template = (
+const template = (
     <div>
         <h1>{app.title}</h1>
         {app.subtitle && <p>{app.subtitle}</p>}
@@ -18,25 +18,35 @@ var template = (
     </div>
 );
 
-var user = {
-    name: "Houston",
-    age: 29,
-    location: "Georgia"
+let count = 0;
+
+const addOne = () => {
+    count++;
+    console.log("addOne", count);
 }
 
-function getLocation(location) {
-    if (location) {
-        return <p>Location: {location}</p>;
-    }
+const minusOne = () => {
+    count--;
+    console.log("minusOne");
 }
 
-var templateTwo = (
+function reset() {
+    count = 0;
+    console.log("reset");
+}
+
+const templateTwo = (
     <div>
-        <h1>{user.name ? user.name : "Anonymous"}</h1>
-        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
+        <h1>
+            Count: {count}
+        </h1>
+        <button onClick={addOne}>+1</button>
+        <button onClick={minusOne}>-1</button>
+        <button onClick={reset}>Reset</button>
     </div>
 );
-var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+console.log(templateTwo);
+
+const appRoot = document.getElementById("app");
+ReactDOM.render(templateTwo, appRoot);
