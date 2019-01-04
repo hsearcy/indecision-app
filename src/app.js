6d1056e064de55a8fe5a4d9a3e4ec9786a4802e0
Houston <houston.searcy@gatherhere.com>
2019-01-04 10:48:42 -0500

    challonge

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import anythingIWant, { square, add } from "./utils.js";
-import { isAdult, canDrink } from "./person.js";
+import isSenior, { isAdult, canDrink } from "./person.js";
 console.log("app.js is running");
 
-console.log(square(4));
-console.log(add(100, 23));
-console.log(anythingIWant(100, 81));
+// console.log(square(4));
+// console.log(add(100, 23));
+// console.log(anythingIWant(100, 81));
 
-// console.log(isAdult(53));
-// console.log(canDrink(21));
+console.log(isAdult(53));
+console.log(canDrink(21));
+console.log(isSenior(18));
--- src/person.js
+++ src/person.js
@@ -1,2 +1,6 @@
 export const isAdult = (age) => age >= 18;
 export const canDrink = (age) => age >= 21;
+
+const isSenior = (age) => age >= 65;
+
+export default isSenior;
