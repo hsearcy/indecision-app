316626f4c971aaaaed1830f308e70c954c1c7cc3
Houston <houston.searcy@gatherhere.com>
2019-01-04 10:46:10 -0500

    more export permutations

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import subtract, { square, add } from "./utils.js";
+import anythingIWant, { square, add } from "./utils.js";
 import { isAdult, canDrink } from "./person.js";
 console.log("app.js is running");
 
 console.log(square(4));
 console.log(add(100, 23));
-console.log(subtract(100, 81));
+console.log(anythingIWant(100, 81));
 
 // console.log(isAdult(53));
 // console.log(canDrink(21));
