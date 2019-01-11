df7cf3758a2e2949e654a5da5ad4f91f3c5436b3
Houston <houston.searcy@gatherhere.com>
2019-01-11 13:56:46 -0500

    start a new thing

--- src/app.js
+++ src/app.js
@@ -2,4 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+const Layout = () => {
+    return (
+        <div>
+            <p>header</p>
+            <p>footer</p>
+        </div>
+    );
+};
+
+const template = (
+    <div>
+        <h1>Page Title</h1>
+        <p>This is my page</p>
+    </div>
+);
+
+ReactDOM.render(<Layout content={template}/>, document.getElementById("app"));
